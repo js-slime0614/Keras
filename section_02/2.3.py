5c9f72766bf04f43b1ef7b4869f8d8a57aa22560
단단한슬라임 <66393239+1409-pjs@users.noreply.github.com>
2021-03-22 19:36:55 +0900

    keras

---
+++ section_02/2.3.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+#원소별 연산
+
+def native_relu(x):
+    assert len(x.shape) == 2    #x 는 넘파이 배열
+    
+    x = x.copy()
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, j] = max(x[i,j], 0)
+    return x
+
+def naive_add(x,y):
+    assert len(x.shape) == 2 #x와 y는 2D 넘파이 배열입니다.
+    assert x.shape == y.shape
+
+    x = x.copy()
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, j] += y[i, j]
+    return x
